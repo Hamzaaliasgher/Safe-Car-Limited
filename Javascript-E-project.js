1f132ad76e81d84b24e42da6c2a553bd204f3f98
Hamza Ali Asgher <114922299+Hamzaaliasgher@users.noreply.github.com>
2023-11-04 18:33:08 +0500

    Add files via upload

---
+++ Javascript-E-project.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+function tw()
+	{
+		
+		document.getElementById("divImage").src="two wheeler.png";
+	}
+function f()
+	{
+		document.getElementById("divImage").src="four wheeler.png";
+	}
+function Third()
+	{
+		document.getElementById("divImage").src="third-party-to-company.png";
+	}
+function T()
+	{
+		document.getElementById("divImage").src="3T-to-company.png";
+	}
+function Comp()
+	{
+		document.getElementById("divImage").src="comprehensive-to-company.png";
+	}
+function Chairman()
+	{
+		document.getElementById("if1").src="chairman.png";
+	}
+function Chiefexicutive()
+	{
+		document.getElementById("if1").src="exicutive.png";
+	}
+function Director1()
+	{
+		document.getElementById("if1").src="director.png";
+	}
+function Director2()
+	{
+		document.getElementById("if1").src="director2.png";
+	}
+function Director3()
+	{
+		document.getElementById("if1").src="director3.png";
+	}
+
+
